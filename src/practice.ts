a1717dbba9489ba2f2433aa9f13ba08ff22b59d7
wansiklee <dhkstlr2662@gmail.com>
2020-05-12 23:55:46 +0900

    함수와 클래스에서 제네릭 사용

--- src/practice.ts
+++ src/practice.ts
@@ -45,3 +45,37 @@ class Rectangle implements Shape {
 const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
 
 shapes.forEach((shape) => console.log(shape.getArea()));
+
+// 제네릭
+interface Items<T> {
+  list: T[];
+}
+
+const items: Items<string> = {
+  list: ["a", "b", "c"],
+};
+
+class Queue<T> {
+  list: T[] = [];
+  get length() {
+    return this.list.length;
+  }
+  enqueue(item: T) {
+    this.list.push(item);
+  }
+  dequeue() {
+    return this.list.shift();
+  }
+}
+
+const queue = new Queue<number>();
+queue.enqueue(0);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
